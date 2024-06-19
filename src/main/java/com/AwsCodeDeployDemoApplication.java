20c413a5e70e9c81dfd60aced14739e109a54a89
MaruthiShankarG <160142741+MaruthiShankarG@users.noreply.github.com>
2024-06-19 16:42:50 -0400

    Update AwsCodeDeployDemoApplication.java

--- src/main/java/com/AwsCodeDeployDemoApplication.java
+++ src/main/java/com/AwsCodeDeployDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.solution.HelloController", "com.solution.AwsCodeDeployDemoApplication"})
+@ComponentScan(basePackages = "com.solution.HelloController," + "com.solution.AwsCodeDeployDemoApplication")
 	public class AwsCodeDeployDemoApplication {
 
 	public static void main(String[] args) {
